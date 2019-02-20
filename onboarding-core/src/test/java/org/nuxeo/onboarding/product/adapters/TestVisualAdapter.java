4773d27514e2d87953142a3327a5a15ce267e627
Samuel Fialho <sfialho@nuxeo.com>
2019-02-20 12:50:58 +0000

    cleanup

--- onboarding-core/src/test/java/org/nuxeo/onboarding/product/adapters/TestVisualAdapter.java
+++ onboarding-core/src/test/java/org/nuxeo/onboarding/product/adapters/TestVisualAdapter.java
@@ -17,7 +17,7 @@
  *     Samuel Fialho
  */
 
-package org.nuxeo.onboarding.product;
+package org.nuxeo.onboarding.product.adapters;
 
 import javax.inject.Inject;
 
@@ -27,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.test.CoreFeature;
+import org.nuxeo.onboarding.product.OnboardingTestFeature;
 import org.nuxeo.onboarding.product.adapters.VisualAdapter;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
