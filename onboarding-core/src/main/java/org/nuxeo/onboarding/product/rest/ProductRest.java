bd94540a2d9f708c073b7ac9501a850df6c0b485
Samuel Fialho <sfialho@nuxeo.com>
2019-02-25 15:29:01 +0000

    rest api stable

--- onboarding-core/src/main/java/org/nuxeo/onboarding/product/rest/ProductRest.java
+++ onboarding-core/src/main/java/org/nuxeo/onboarding/product/rest/ProductRest.java
@@ -1,12 +1,32 @@
 package org.nuxeo.onboarding.product.rest;
 
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+import org.nuxeo.onboarding.product.adapters.ProductAdapter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
+@WebObject(type = "product")
+@Produces("application/json")
 @Path("product")
-public class ProductRest {
+public class ProductRest extends ModuleRoot {
     @GET
-    public Object doGet() {
-        return "Hello World!";
+    @Path("price/{productId}")
+    public String getProductAndReturnPrice(@PathParam("productId") String productId) {
+        return computePrice(new IdRef(productId));
+    }
+
+    private String computePrice(DocumentRef documentRef) {
+        DocumentModel product = getContext().getCoreSession().getDocument(documentRef);
+        ProductAdapter productAdapter = product.getAdapter(ProductAdapter.class);
+        Double price = productAdapter.getPrice();
+        return Double.toString(price);
     }
 }
+
